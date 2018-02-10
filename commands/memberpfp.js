dc5dd2a3e0c8526343138dedda309cbf680f7f30
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-10 17:00:10 -0500

    Create memberpfp.js

---
+++ commands/memberpfp.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let you = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!you) return message.channel.send((`${message.author}, You must mention a user!`))
+    return message.channel.send(`https://cdn.discordapp.com/avatars/${you.id}/${you.avatar}.png`);
+
+}
+module.exports.help = {
+    name: "memberpfp"
+}

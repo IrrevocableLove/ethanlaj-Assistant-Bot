9115cecfcc148b3000d6b26ce4b591c0d22369ed
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-10 11:41:36 -0500

    Update formatad.js

--- commands/formatad.js
+++ commands/formatad.js
@@ -1,4 +1,9 @@
 module.exports.run = async (bot, message, args) => {
+let userid = message.author.id
+let required = "245877990938902529"
+if (userid!==required) {
+      console.log("no")
+}
 const title = await bot.awaitReply(message, "Title?", 180000);
 const ad = await bot.awaitReply(message, "Ad?", 180000);
 const thumbnail = await bot.awaitReply(message, "Thumbnail?", 180000);

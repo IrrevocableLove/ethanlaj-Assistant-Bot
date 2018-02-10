8722bbac94c73fc3bb03ed71920a8dc71bacd93c
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-10 10:16:11 -0500

    Update formatad.js

--- commands/formatad.js
+++ commands/formatad.js
@@ -1,5 +1,3 @@
-const Discord = require("discord.js");
-
 module.exports.run = async (bot, message, args) => {
 const title = await bot.awaitMessage(message, "Title?", 180000);
 const ad = await bot.awaitMessage(message, "Ad?", 180000);

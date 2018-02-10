fb0bed9bcdf3444fccfcc235da6dc5a0b3c146fa
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-10 09:15:55 -0500

    Create brotimeinvite.js

---
+++ commands/brotimeinvite.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+return message.channel.send("https://www.discord.gg/rjM8wdZ")
+
+
+
+}
+module.exports.help = {
+      name: "brotimeinvite"
+  }
+

a37a41ca3365c5f4ea35057be543459f2a6b1e9f
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-10 09:10:10 -0500

    Add files via upload

---
+++ commands/cbrotimead.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+
+return message.channel.send("```__Bro Time__ \nWant to join a **fun**, **engaging** community? Do you enjoy participating in **giveaways**, posting **memes**, creating **stories**, talking with other **developers**, Playing **games** with bots, listening to **music**, joining **game nights**, or answering **QOTDs**? \nThen You Should **Join The Community Today!** \nhttps://discord.gg/rjM8wdZ```")
+
+return message.channel.send("")
+
+
+
+
+}
+module.exports.help = {
+      name: "cbrotimead"
+  }

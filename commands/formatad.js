da525548a7bc051d06e121703f3ae498bbf6f4f4
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-10 12:15:20 -0500

    Update formatad.js

--- commands/formatad.js
+++ commands/formatad.js
@@ -3,6 +3,7 @@ let required = "245877990938902529"
 if (message.author.id!==required) {
       return message.channel.send("You're not authorized!");
 }
+const filter = m=>m.author.id = "245877990938902529"
 const title = await bot.awaitReply(message, "Title?", 180000);
 const ad = await bot.awaitReply(message, "Ad?", 180000);
 const thumbnail = await bot.awaitReply(message, "Thumbnail?", 180000);

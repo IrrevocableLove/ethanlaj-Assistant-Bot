39fe3a9a22063b7d8bf65481936b0aa5dd2abaff
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-10 11:29:09 -0500

    Update formatad.js

--- commands/formatad.js
+++ commands/formatad.js
@@ -1,8 +1,9 @@
 module.exports.run = async (bot, message, args) => {
-const title = await bot.awaitMessage(message, "Title?", 180000);
-const ad = await bot.awaitMessage(message, "Ad?", 180000);
+const title = await bot.awaitReply(message, "Title?", 180000);
+const ad = await bot.awaitReply(message, "Ad?", 180000);
+const thumbnail = await bot.awaitReply(message, "Thumbnail?", 180000);
 
-message.channel.send(`${title}, ${ad}`)
+message.channel.send(`.custom edit eaa 1 embed:title:"${title}",description:"${ad}",\ncolor:"orange",thumbnail:"${thumbnail}"`)
 
 
 

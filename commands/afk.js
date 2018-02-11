55ebb54409615c5cbd60064dbcfc7083af688eb7
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-11 17:01:27 -0500

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -1,14 +1,14 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-  if(!client.afk) {
-  client.afk = true
   let afkstat = args.join(" ")
-  client.afkmsg = `**ethanlaj's AFK Message:** ${afkstat}`
-  message.react(":onswitch:412046075105640448")
-  } else {
+  if(!afkstat) {
   client.afk = false;
   message.react(":offswitch:412046076988882944")
+  } else {
+  client.afk = true
+  client.afkmsg = `**ethanlaj's AFK Message:** ${afkstat}`
+  message.react(":onswitch:412046075105640448")
   }
 
 

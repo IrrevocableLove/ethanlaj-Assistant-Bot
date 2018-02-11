b5c9e5dbd4dff4e3a0282e68681b3909111aa9d2
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-10 19:46:53 -0500

    Update delete.js

--- commands/delete.js
+++ commands/delete.js
@@ -3,10 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
   if(!client.wowdown) {
   client.wowdown = true
-    message.react("<:onswitch:412046075105640448>");
+    message.react(":onswitch:412046075105640448");
   } else {
   client.wowdown = false
-    message.react("<:offswitch:412046076988882944>");
+    message.react(":offswitch:412046076988882944");
 
   }
 

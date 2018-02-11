d8022c8662feee25c04c608a0c3c9fb2ae70ed09
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-11 16:54:36 -0500

    Update delete.js

--- commands/delete.js
+++ commands/delete.js
@@ -3,9 +3,11 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
   if(!client.wowdown) {
   client.wowdown = true
+  client.delchannel = message.channel.id
     message.react(":onswitch:412046075105640448");
   } else {
   client.wowdown = false
+  client.delchannel = "None"
     message.react(":offswitch:412046076988882944");
 
   }

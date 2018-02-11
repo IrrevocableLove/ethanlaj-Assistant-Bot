33e7c59ebff8e60743917652524ecc1836e3bc68
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-10 19:29:47 -0500

    Update delete.js

--- commands/delete.js
+++ commands/delete.js
@@ -3,15 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
   if(!client.wowdown) {
   client.wowdown = true
-    message.react("\u004F")
-    message.react("\u006E")
     message.react("\u2705");
   } else {
   client.wowdown = false
-    message.react("\u004F")
-    message.react("\u006F")
-    message.react("\u0066")
-    message.react("\u2705");
+    message.react(":x:");
 
   }
 

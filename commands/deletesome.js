bd80d48b1974fc5eeb2133f95132b9caa6bf4efb
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-10 19:16:48 -0500

    Update deletesome.js

--- commands/deletesome.js
+++ commands/deletesome.js
@@ -5,6 +5,7 @@ module.exports.run = async (client, message, args) => {
   client.wowdown = true
     message.react("\u004F")
     message.react("\u006E")
+    message.react("\u2705");
   } else {
   client.wowdown = false
     message.react("\u004F")

c4d207694c7e7072a4e5fab5917a7266ff849b08
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-11 08:26:34 -0500

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let busrer = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    let buser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!buser) return message.channel.send(`${message.author}, Please mention a user!`);
    
    

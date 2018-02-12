9482488d0f50c38486e83027b4e27e3e82c29b1d
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-11 19:35:50 -0500

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
   message.react(":offswitch:412046076988882944")
   } else {
   client.afk = true
-  client.afkmsg = `**ethanlaj's:** ${afkstat}`
+  client.afkmsg = `**ethanlaj:** ${afkstat}`
   message.react(":onswitch:412046075105640448")
   }
 

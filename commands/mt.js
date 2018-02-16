ed647026529864b2ee7ac20355ccc8a2d50f3450
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-16 16:52:58 -0500

    Update mt.js

--- commands/mt.js
+++ commands/mt.js
@@ -6,6 +6,7 @@ module.exports.run = async (bot, message, args) => {
     let role = message.guild.roles.find(r=> r.name.toLowerCase() === prefix+rolename);
     if(role) role.setMentionable(!role.mentionable);
   }
+  message.delete()
 };
 
 module.exports.help = {
